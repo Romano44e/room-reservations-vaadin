58322e2fc48710d835a2d9aaab566fa1a5b6b4b7
Roman <roman.adamczyk12@gmail.com>
2025-04-23 13:06:33 +0200

    first commit

---
+++ src/main/java/com/room_reservations/RoomReservationsApplication.java
@@ -0,0 +1,13 @@
+package com.room_reservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomReservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomReservationsApplication.class, args);
+	}
+
+}
